8a1b248aeafde03bf609f146e98539c666822bcd
JohnWalkerQ <mikael9998@hotmail.no>
2016-08-23 19:57:09 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -48,8 +48,8 @@
     localStorage.setItem("basicBotsettings", JSON.stringify({
       botName: "LNG Bot",
       language: "english",
-      chatLink: "https://rawgit.com/basicBot/source/master/lang/en.json",
-      scriptLink: "https://rawgit.com/basicBot/source/master/basicBot.js",
+      chatLink: "https://rawgit.com/JohnWalkerQ/lngbot/master/lang/en.json",
+      scriptLink: "https://rawgit.com/JohnWalkerQ/lngbot/master/extension.js",
       roomLock: false, // Requires an extension to re-load the script
       startupCap: 1, // 1-200
       startupVolume: 0, // 0-100
@@ -105,9 +105,9 @@
       songstats: true,
       commandLiteral: "!",
       blacklists: {
-        NSFW: "https://rawgit.com/basicBot/custom/master/blacklists/NSFWlist.json",
-        OP: "https://rawgit.com/basicBot/custom/master/blacklists/OPlist.json",
-        BANNED: "https://rawgit.com/basicBot/custom/master/blacklists/BANNEDlist.json"
+        NSFW: "https://rawgit.com/JohnWalkerQ/lngbot/master/blacklists/NSFWlist.json",
+        OP: "https://rawgit.com/JohnWalkerQ/lngbot/master/blacklists/OPlist.json",
+        BANNED: "https://rawgit.com/JohnWalkerQ/lngbot/master/blacklists/BANNEDlist.json"
       }
     }));
 
